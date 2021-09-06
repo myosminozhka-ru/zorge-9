d7235a79c9f8fbbe62269cd6cfe61cb9eff0eaee
Sergey Goncharovр <doimpos@gmail.com>
2021-09-06 22:21:29 +0500

    тест

--- html/src/js/import/modules.js
+++ html/src/js/import/modules.js
@@ -1,4 +1,5 @@
 import "%modules%/header/header";
 import "%modules%/main_blocks/main_blocks";
+import "%modules%/main_pref/main_pref";
 import "%modules%/main_contacts/main_contacts";
 import "%modules%/footer/footer"
