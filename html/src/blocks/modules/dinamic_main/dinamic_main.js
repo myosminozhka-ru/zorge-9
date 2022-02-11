d3bd4d5cf115a9b1b0e027d33aa61e3e7b4c8346
Ruslan Garapov <ruslangarapov1994@gmail.com>
2022-02-11 17:26:22 +0300

    Merge branch master of bitbucket.org:osminozhka/zorge-9

--- html/src/blocks/modules/dinamic_main/dinamic_main.js
+++ html/src/blocks/modules/dinamic_main/dinamic_main.js
@@ -3,13 +3,10 @@ $(function() {
     $('.switch').change(function() {
         $('.dinamic_main__bottom').toggleClass('active');
     });
-<<<<<<< HEAD
     $('.block_photo').on('click', function() {
         $('.dinamic_main__bottom').removeClass('active');
     })
     $('.block_video').on('click', function() {
         $('.dinamic_main__bottom').addClass('active');
     })
-=======
->>>>>>> dc63f5c128d0dd1edc20543668a5852efb14a5cc
 })
